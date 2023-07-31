14fe43c865d5cd3a6b954c33df34dabc203f89f0
Cameron Garrison <cgarriso@redhat.com>
2023-07-31 14:22:13 -0400

    Update pkg/kfapp/ossm/types.go
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- pkg/kfapp/ossm/types.go
+++ pkg/kfapp/ossm/types.go
@@ -65,6 +65,10 @@ func ReplaceChar(s string, oldChar, newChar string) string {
 	return strings.ReplaceAll(s, oldChar, newChar)
 }
 
+// In order to process the templates, we need to create a tmp directory
+// to store the files. This is because embedded files are read only.
+// copyEmbeddedFS ensures that files embedded using go:embed are populated
+// to dest directory
 func copyEmbeddedFS(fsys fs.FS, root, dest string) error {
 	return fs.WalkDir(fsys, root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
